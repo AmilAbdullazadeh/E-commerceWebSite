6188c11b75ea8f3a1ce1e49e8854a2e1672338ef
AmilAbdullazadeh <amilva@code.edu.az>
2019-09-18 15:49:58 +0400

    new changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
-$(window).on('load', function(){
+$(window).on('load', function () {
 
     console.log("Onload is working !");
 
-     $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="tooltip"]').tooltip();
+
+    // $('.card-img').slick({
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     arrows: false,
+    //     fade: true,
+    //     asNavFor: '.slider'
+    // });
+    // $('.slider').slick({
+    //     slidesToShow: 3,
+    //     slidesToScroll: 1,
+    //     asNavFor: '.card-img',
+    //     dots: true,
+    //     centerMode: true,
+    //     focusOnSelect: true
+    // });
 });
